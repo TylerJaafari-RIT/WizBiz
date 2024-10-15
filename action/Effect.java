3d50ab2ca2b0d638ad6cac764fae7b2ff789a02f
Tyler <txj3401@glados.cs.rit.edu>
2024-10-15 19:11:32 -0400

    Added documentation to Effect interface.

--- action/Effect.java
+++ action/Effect.java
@@ -3,6 +3,14 @@ package action;
 import creatures.Character;
 
 public interface Effect {
+	/**
+	 * Handles duration decrementing and effects over time.
+	 */
 	public void passTurn();
+
+	/**
+	 * Clones this Effect or EffectComposites onto the specified target.
+	 * @param target
+	 */
 	public void inflict(Character target);
 }
